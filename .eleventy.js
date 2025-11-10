08740c1d69ae6e828b6873db75f81584f3217e8d
tunan-pan <117625483+tunan-pan@users.noreply.github.com>
2025-11-09 23:51:17 -0800

    project reordering code

--- .eleventy.js
+++ .eleventy.js
@@ -16,6 +16,26 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addCollection("projects", function(collectionApi) {
     return collectionApi.getFilteredByGlob("projects/*.md");
   });
+
+  eleventyConfig.addCollection("sortedProjects", function(collectionApi) {
+  const projectOrder = require('./_data/projectOrder.json');
+  const projects = collectionApi.getFilteredByGlob("projects/*.md");
+  
+  // Log the slugs so you can see them
+  projects.forEach(p => console.log("File slug:", p.fileSlug));
+  
+  return projects.sort((a, b) => {
+    const aSlug = a.fileSlug;
+    const bSlug = b.fileSlug;
+    const aIndex = projectOrder.indexOf(aSlug);
+    const bIndex = projectOrder.indexOf(bSlug);
+    
+    if (aIndex === -1) return 1;
+    if (bIndex === -1) return -1;
+    
+    return aIndex - bIndex;
+  });
+});
   
   return {
     dir: {
