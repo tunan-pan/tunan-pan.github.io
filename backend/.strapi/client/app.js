5e72b42e275d25424f0ad9e0045d5144a087fecb
tunan-pan <117625483+tunan-pan@users.noreply.github.com>
2026-04-27 16:43:38 -0700

    SAS project

--- backend/.strapi/client/app.js
+++ backend/.strapi/client/app.js
@@ -3,7 +3,6 @@
  * Any modifications made will be discarded.
  */
 import ckeditor5 from "@_sh/strapi-plugin-ckeditor/strapi-admin";
-import ckeditor from "@ckeditor/strapi-plugin-ckeditor/strapi-admin";
 import strapiCloud from "@strapi/plugin-cloud/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
 import { renderAdmin } from "@strapi/strapi/admin";
@@ -11,7 +10,6 @@ import { renderAdmin } from "@strapi/strapi/admin";
 renderAdmin(document.getElementById("strapi"), {
   plugins: {
     ckeditor5: ckeditor5,
-    ckeditor: ckeditor,
     "strapi-cloud": strapiCloud,
     "users-permissions": usersPermissions,
   },
